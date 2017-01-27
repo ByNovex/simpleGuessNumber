ff82bdb3af33423d6e4805f290b9d5916253b92a
Byron Bacusoy Pinela <ByronBP@macbook-pro-de-byron.local>
2017-01-27 20:59:40 +0100

    Initial Commit

---
+++ simpleGuessNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleGuessNumber
+//
+//  Created by Byron Bacusoy Pinela on 27/1/17.
+//  Copyright © 2017 Novex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
