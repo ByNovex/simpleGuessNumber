a51b4d52e8c39b2c54bbc21750a53addf45403b4
Byron Bacusoy Pinela <ByronBP@MacBook-Pro-de-Byron.local>
2017-02-10 17:35:09 +0100

    cleanIt

--- simpleGuessNumber/ViewController.swift
+++ simpleGuessNumber/ViewController.swift
@@ -9,76 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    //-------------------------FOR BACKGROUND-------------------------
-    @IBOutlet weak var background: UIImageView!
-    var pointStart = CGPoint(x: 0.0, y: 0.0)
-    var pointEnd = CGPoint(x: 1.0, y: 1.0)
-    var phase = 0
-    let gradient = CAGradientLayer()
-
-    func backgroundEffect()
-    {
-        getCurrentPhase()
-        switch phase {
-        case 1:
-            phaseOne()
-        case 2:
-            phaseTwo()
-        case 3:
-            phaseThree()
-        default:
-            phaseFour()
-        }
-        fixBugDouble(Phase: phase)
-        redrawBackground()
+    //--------------------------------------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initApp()
     }
-    func getCurrentPhase()
-    {
-        if (pointStart.equalTo(CGPoint(x: 0.0, y: 0.0)) && pointEnd.equalTo(CGPoint(x: 1.0, y: 1.0))) {phase = 1}
-        if (pointStart.equalTo(CGPoint(x: 1.0, y: 0.0)) && pointEnd.equalTo(CGPoint(x: 0.0, y: 1.0))) {phase = 2}
-        if (pointStart.equalTo(CGPoint(x: 1.0, y: 1.0)) && pointEnd.equalTo(CGPoint(x: 0.0, y: 0.0))) {phase = 3}
-        if (pointStart.equalTo(CGPoint(x: 0.0, y: 1.0)) && pointEnd.equalTo(CGPoint(x: 1.0, y: 0.0))) {phase = 4}
+    func initApp(){
+        initBackground()
+        initGame()
     }
-    func phaseOne()
-    {
-        pointStart.x += 0.1
-        pointEnd.x -= 0.1
-    }
-    func phaseTwo()
-    {
-        pointStart.y += 0.1
-        pointEnd.y -= 0.1
-    }
-    func phaseThree()
-    {
-        pointStart.x -= 0.1
-        pointEnd.x += 0.1
-        
-    }
-    func phaseFour()
-    {
-        pointStart.y -= 0.1
-        pointEnd.y += 0.1
-    }
-    func redrawBackground()
-    {
-        gradient.startPoint = pointStart
-        gradient.endPoint = pointEnd
-    }
-    func fixBugDouble(Phase:Int)
-    {//Parece ser que no se suma/resta 0.1, si no que un poco más. Así que nunca llegarán a ser exactamente 0.0 o 1.0
-        switch Phase {
-        case 1:
-            if(pointEnd.x < 0.1) {pointStart.x = 1.0;pointEnd.x = 0.0}
-        case 2:
-            if (pointEnd.y < 0.1) {pointStart.y = 1.0;pointEnd.y = 0.0}
-        case 3:
-            if (pointStart.x < 0.1) {pointStart.x = 0.0; pointEnd.x = 1.0}
-        default:
-            if (pointStart.y < 0.1) {pointStart.y = 0.0; pointEnd.y = 1.0}
-        }
-    }
+    //--------------------------------------------------------------
+    //----------------------------GAME------------------------------
     //--------------------------------------------------------------
     //VARIABLES
     //--------------------------------------------------------------
@@ -92,10 +33,6 @@ class ViewController: UIViewController {
     //--------------------------------------------------------------
     //OBJETOS
     //--------------------------------------------------------------
-    @IBOutlet weak var titleLabelConstraintY: NSLayoutConstraint!
-    @IBOutlet weak var tryLabelConstraintY: NSLayoutConstraint!
-    @IBOutlet weak var rightButtonConstraintY: NSLayoutConstraint!
-
     @IBOutlet weak var stackButtons: UIStackView!
     @IBOutlet weak var okButton: customButton!
     @IBOutlet weak var rightButton: customButton!
@@ -104,14 +41,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var infoLabel: customLabel!
     @IBOutlet weak var tryLabel: customLabel!
     @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var titleLabelConstraintY: NSLayoutConstraint!
+    @IBOutlet weak var tryLabelConstraintY: NSLayoutConstraint!
+    @IBOutlet weak var rightButtonConstraintY: NSLayoutConstraint!
     //--------------------------------------------------------------
-    //FUNCIONES BUTTON
+    //BUTTONSFUNCTIONS
     //--------------------------------------------------------------
     @IBAction func okButtonAction(_ sender: AnyObject) {
-        animationOKButtonPressed()
+        infoToGameAnimation()
         calculateNum()
     }
-    
     @IBAction func highButtonAction(_ sender: AnyObject) {
         minNumber = avgNumber
         calculateNum()
@@ -121,80 +61,66 @@ class ViewController: UIViewController {
         calculateNum()
     }
     @IBAction func rightButtonAction(_ sender: AnyObject) {
-        if (isAnotherOneText) {anotherOneGame()}
+        if (isAnotherOneText) {endToInfoAnimation()}
         else {endGame(userWins: false)}
     }
     //--------------------------------------------------------------
     //FUNCIONES
     //--------------------------------------------------------------
-    
-    func initApp(){
-        initBackground()
-        initGame()
-    }
-    func initBackground(){
-        gradient.frame = view.bounds
-        gradient.colors = [UIColor.red.cgColor,UIColor.yellow.cgColor,UIColor.green.cgColor,UIColor.blue.cgColor]
-        gradient.endPoint = pointEnd
-        gradient.startPoint = pointStart
-        background.layer.insertSublayer(gradient, at: 0)
-        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(backgroundEffect), userInfo: nil, repeats: true)
-    }
     func initGame() {
-        setAlphaTo0()
-        getMinMax()
-        setInfo()
-        titleAnimation()
-    }
-    
-    func anotherOneGame() {
-        prepareAnotherGame()
-        UIView.animate(withDuration: 0.3, animations: {
-            self.setAlphaTo0()
-            self.view.layoutIfNeeded()
-        }) { (true) in
-            self.prepareGame()
-            UIView.animate(withDuration: 0.3, animations: {
-                self.view.layoutIfNeeded()
-                }, completion: { (true) in
-                    self.appearInfoLabelOkButton()
-                    
-            })
-        }
-        
+        getConstantConstraints()
+        prepareGame()
+        startToInfoAnimation()
     }
     func endGame(userWins:Bool){
-        setTextEndGame(win: userWins)
-        endGameAnimation()
+        setEndGameText(win: userWins)
+        gameToEndAnimation()
     }
-    
-    
-    
-    func prepareAnotherGame(){
-        getMinMax()
-        tryNumber = 0
-        isAnotherOneText = false
-        okButton.isEnabled = true
+    //--------------------------------------------------------------
+    func startToInfoAnimation() {
+        titleLabel.center.y = 1000
+        UIView.animate(withDuration: 2.0, animations: {
+            self.moveTitleLabelToCenter()}){
+                (true) in
+                UIView.animate(withDuration: 1.5, animations: {
+                    self.moveTitleLabeToTop()},completion: {
+                        (true) in
+                        self.appearInfoLabelOkButton()
+                })
+        }
     }
+    func infoToGameAnimation() {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.desappearOKButton()}) {
+                (true) in
+                self.moveTryLabel()
+                self.appearGameTemplate()
+        }
+    }
+    func gameToEndAnimation(){
+        UIView.animate(withDuration: 1.0, animations: {
+            self.desappearStackButtonsNumberLabel()}){
+                (true) in
+                UIView.animate(withDuration: 1.0, animations: {
+                    self.moveRightButton()})
+                    self.setRightButtonTextAnotherOne()
+        }
+    }
+    func endToInfoAnimation() {
+        UIView.animate(withDuration: 1.5, animations: {
+            self.setAlphaTo0ButTitle()
+        }) { (true) in
+            self.prepareGame()
+            self.appearInfoLabelOkButton()
+
+        }
+    }
+    //--------------------------------------------------------------
     func prepareGame() {
-        self.setInfo()
-        self.rightButtonConstraintY.constant = self.constantRightButtonConstraintY
-        self.tryLabelConstraintY.constant = self.constantTryLabelConstraintY
-        self.rightButton.setTitle("Correcto!", for: .normal)
-    }
-    
-    func changeRightButtonText() {
-        self.rightButton.setTitle("Otra?", for: .normal)
-        self.isAnotherOneText = true
-    }
-    func setTextEndGame(win:Bool){
-        if(win){tryLabel.text = "YOU WIN!"}
-        else {tryLabel.text = "Im the best"}
-    }
-    func setNumberAndTry(){
-        tryLabel.text = "\(tryNumber)"
-        avgNumber = Int((maxNumber + minNumber)/2)
-        numberLabel.text = "\(avgNumber)"
+        getMinMaxNumbers()
+        setInfo()
+        setDefaultValues()
+        setAlphaTo0ButTitle()
     }
     func calculateNum(){
         tryNumber += 1
@@ -204,22 +130,6 @@ class ViewController: UIViewController {
         else{
             endGame(userWins: true)
         }
-    }
-    func setAlphaTo0(){
-        okButton.alpha = 0.0
-        infoLabel.alpha = 0.0
-        tryLabel.alpha = 0.0
-        numberLabel.alpha = 0.0
-        rightButton.alpha = 0.0
-        stackButtons.alpha = 0.0
-    }
-    func setInfo(){
-        infoLabel.text = "Piensa en un número entre el \(minNumber) y el \(maxNumber) y lo adivinaré en menos de 10 intentos"
-    }
-    func getMinMax() {
-        minNumber = Int(arc4random_uniform(1000))
-        maxNumber = Int(arc4random_uniform(1000))
-        checkMinMax()
     }
     func checkMinMax(){
         if (minNumber > maxNumber){
@@ -237,84 +147,187 @@ class ViewController: UIViewController {
             }
         }
     }
-    
-    
-    func desappearStackButtonsNumberLabel(){
-        self.stackButtons.alpha = 0.0
-        self.numberLabel.alpha = 0.0
+    //--------------------------------------------------------------
+    func setInfo(){
+        infoLabel.text = "Piensa en un número entre el \(minNumber) y el \(maxNumber) y lo adivinaré en menos de 10 intentos"
+    }
+    func setDefaultValues(){
+        tryNumber = 0
+        okButton.isEnabled = true
+        setRightButtonTextRight()
+        setConstraintToConstant()
+    }
+    func setAlphaTo0ButTitle(){
+        okButton.alpha = 0.0
+        infoLabel.alpha = 0.0
+        tryLabel.alpha = 0.0
+        numberLabel.alpha = 0.0
+        rightButton.alpha = 0.0
+        stackButtons.alpha = 0.0
+    }
+    func setEndGameText(win:Bool){
+        if(win){tryLabel.text = "YOU WIN!"}
+        else {tryLabel.text = "Im the best"}
+    }
+    func setNumberAndTry(){
+        avgNumber = Int((maxNumber + minNumber)/2)
+        numberLabel.text = "\(avgNumber)"
+        tryLabel.text = "\(tryNumber)"
+    }
+    func setRightButtonTextRight(){
+        self.rightButton.setTitle("Correcto!", for: .normal)
+        isAnotherOneText = false
+    }
+    func setRightButtonTextAnotherOne() {
+        self.rightButton.setTitle("Otra?", for: .normal)
+        self.isAnotherOneText = true
+    }
+    func setConstraintToConstant() {
+        self.rightButtonConstraintY.constant = self.constantRightButtonConstraintY
+        self.tryLabelConstraintY.constant = self.constantTryLabelConstraintY
+    }
+    func getMinMaxNumbers() {
+        minNumber = Int(arc4random_uniform(1000))
+        maxNumber = Int(arc4random_uniform(1000))
+        checkMinMax()
+    }
+    func getConstantConstraints(){
+        constantRightButtonConstraintY = rightButtonConstraintY.constant
+        constantTryLabelConstraintY = tryLabelConstraintY.constant
+    }
+    //--------------------------------------------------------------
+    func moveTitleLabelToCenter(){
+        view.layoutIfNeeded()
+    }
+    func moveTitleLabeToTop() {
+        titleLabelConstraintY.constant = 5
+        view.layoutIfNeeded()
     }
     func moveRightButton() {
-        self.rightButtonConstraintY.constant = 5
-        self.view.layoutIfNeeded()
+        rightButtonConstraintY.constant = 5
+        view.layoutIfNeeded()
     }
-    func endGameAnimation(){
-        UIView.animate(withDuration: 1.0, animations: {self.desappearStackButtonsNumberLabel()})
-        { (true) in
-            UIView.animate(withDuration: 1.0, animations: {self.moveRightButton()})
-            self.changeRightButtonText()
-        }
+    func moveTryLabel() {
+        tryLabelConstraintY.constant = 5
     }
     func appearInfoLabelOkButton(){
-        UIView.animate(withDuration: 0.5, delay: 1.0, options: .curveEaseIn, animations: {
+        UIView.animate(withDuration: 1.5) {
             self.okButton.alpha = 1.0
             self.infoLabel.alpha = 1.0
-            }, completion: nil)
-    }
-    func titleAnimation() {
-        titleLabel.center.y = 1000
-        UIView.animate(withDuration: 2.0, animations: {
-            self.view.layoutIfNeeded()
-        }) { (true) in
-            self.titleLabelConstraintY.constant = 5
-            UIView.animate(withDuration: 1.5, animations: {
-                self.view.layoutIfNeeded()
-                }, completion: { (true) in
-                    self.appearInfoLabelOkButton()
-            })
         }
     }
-    func animationOKButtonPressed() {
+    func appearGameTemplate(){
         UIView.animate(withDuration: 1.0, animations: {
-            self.desappearOKButton()
-        }) { (true) in
-            self.tryLabelConstraintY.constant = 5
-            UIView.animate(withDuration: 1.0, animations: {
-                self.appearTryLabelNumberLabel()
-                }, completion: { (true) in
-                    self.animationStackButtons()
-            })
-        }
-    }
-    func desappearOKButton(){
-        self.okButton.alpha = 0.0
-        self.okButton.isEnabled = false
+            self.appearTryLabelNumberLabel()}, completion: {
+                (true) in
+                self.appearStackButtons()
+        })
     }
     func appearTryLabelNumberLabel() {
-        self.tryLabel.alpha = 1.0
-        self.numberLabel.alpha = 1.0
+        tryLabel.alpha = 1.0
+        numberLabel.alpha = 1.0
     }
-    func animationStackButtons(){
-        self.stackButtons.alpha = 1.0
-        self.rightButton.alpha = 1.0
-        let aux = self.rightButton.center.x
-        self.stackButtons.center.x = 600
-        self.rightButton.center.x = 600
+    func appearStackButtons(){
+        stackButtons.alpha = 1.0
+        rightButton.alpha = 1.0
+        let aux = rightButton.center.x
+        stackButtons.center.x = 600
+        rightButton.center.x = 600
         UIView.animate(withDuration: 0.3, animations: {
             self.rightButton.center.x = aux
             self.stackButtons.center.x = aux
         })
     }
-    
+    func desappearOKButton(){
+        okButton.alpha = 0.0
+        okButton.isEnabled = false
+    }
+    func desappearStackButtonsNumberLabel(){
+        stackButtons.alpha = 0.0
+        numberLabel.alpha = 0.0
+    }
     //--------------------------------------------------------------
-    //METODOS
+    //-------------------------BACKGROUND---------------------------
     //--------------------------------------------------------------
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    //VARIABLES
+    //--------------------------------------------------------------
+    @IBOutlet weak var background: UIImageView!
+    let gradient = CAGradientLayer()
+    var pointStart = CGPoint(x: 0.0, y: 0.0)
+    var pointEnd = CGPoint(x: 1.0, y: 1.0)
+    var currentPhase = 0
+    //--------------------------------------------------------------
+    //FUNCIONES
+    //--------------------------------------------------------------
+    func initBackground(){
+        createGradient()
+        createBackground()
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(backgroundEffect), userInfo: nil, repeats: true)
+    }
+    //--------------------------------------------------------------
+    func createGradient(){
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.red.cgColor,UIColor.yellow.cgColor,UIColor.green.cgColor,UIColor.blue.cgColor]
+        redrawBackground()
+    }
+    func createBackground(){
+        background.layer.insertSublayer(gradient, at: 0)
+    }
+    //--------------------------------------------------------------
+    func backgroundEffect(){
+        getCurrentPhase()
+        switch currentPhase {
+        case 1:
+            phaseOne()
+        case 2:
+            phaseTwo()
+        case 3:
+            phaseThree()
+        default:
+            phaseFour()
+        }
+        fixBugDouble(Phase: currentPhase)
+        redrawBackground()
+    }
+    func getCurrentPhase(){
+        if (pointStart.equalTo(CGPoint(x: 0.0, y: 0.0)) && pointEnd.equalTo(CGPoint(x: 1.0, y: 1.0))) {currentPhase = 1}
+        if (pointStart.equalTo(CGPoint(x: 1.0, y: 0.0)) && pointEnd.equalTo(CGPoint(x: 0.0, y: 1.0))) {currentPhase = 2}
+        if (pointStart.equalTo(CGPoint(x: 1.0, y: 1.0)) && pointEnd.equalTo(CGPoint(x: 0.0, y: 0.0))) {currentPhase = 3}
+        if (pointStart.equalTo(CGPoint(x: 0.0, y: 1.0)) && pointEnd.equalTo(CGPoint(x: 1.0, y: 0.0))) {currentPhase = 4}
+    }
+    func phaseOne(){
+        pointStart.x += 0.1
+        pointEnd.x -= 0.1
+    }
+    func phaseTwo(){
+        pointStart.y += 0.1
+        pointEnd.y -= 0.1
+    }
+    func phaseThree(){
+        pointStart.x -= 0.1
+        pointEnd.x += 0.1
         
-        constantRightButtonConstraintY = rightButtonConstraintY.constant
-        constantTryLabelConstraintY = tryLabelConstraintY.constant
-        initApp()
-        
+    }
+    func phaseFour(){
+        pointStart.y -= 0.1
+        pointEnd.y += 0.1
+    }
+    func fixBugDouble(Phase:Int){
+        //Parece ser que no se suma/resta 0.1, si no que un poco más. Así que nunca llegarán a ser exactamente 0.0 o 1.0
+        switch Phase {
+        case 1:
+            if(pointEnd.x < 0.1) {pointStart.x = 1.0;pointEnd.x = 0.0}
+        case 2:
+            if (pointEnd.y < 0.1) {pointStart.y = 1.0;pointEnd.y = 0.0}
+        case 3:
+            if (pointStart.x < 0.1) {pointStart.x = 0.0;pointEnd.x = 1.0}
+        default:
+            if (pointStart.y < 0.1) {pointStart.y = 0.0;pointEnd.y = 1.0}
+        }
+    }
+    func redrawBackground(){
+        gradient.startPoint = pointStart
+        gradient.endPoint = pointEnd
     }
     //--------------------------------------------------------------
 }
